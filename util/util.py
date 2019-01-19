6120b0ba9a768d1bf9f995d01e4dc40944b4a7b7
Sambhav Jain <sambhavj@xilinx.com>
2019-01-18 18:35:32 -0800

    misc edits / cleanup

--- util/util.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-
-def none_to_m1(x):
-    return x if x is not None else -1

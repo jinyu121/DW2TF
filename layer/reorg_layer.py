1675813c2e3f822aa10b83fb0a530db30d6b68ce
Sambhav Jain <sambhavj@xilinx.com>
2020-02-03 14:45:56 -0800

    Switched to tf.extract_image_patches for reorg (based on https://github.com/thtrieu/darkflow/blob/master/darkflow/net/ops/convolution.py#L27-L28 and https://github.com/thtrieu/darkflow/issues/173). This is input rescaling friendly, say for HD yolo-v2

--- layer/reorg_layer.py
+++ layer/reorg_layer.py
@@ -11,7 +11,7 @@ def none_to_m1(x):
     return x if x is not None else -1
 
 
-def reorg_layer(net, stride=2, name='reorg'):
+def old_reorg_layer(net, stride=2, name='reorg'):
     batch_size, height, width, channels = net.get_shape().as_list()
     batch_size = none_to_m1(batch_size)
     _height, _width, _channel = height // stride, width // stride, channels * stride * stride
@@ -19,4 +19,14 @@ def reorg_layer(net, stride=2, name='reorg'):
         net = tf.reshape(net, [batch_size, _height, stride, _width, stride, channels])
         net = tf.transpose(net, [0, 1, 3, 2, 4, 5])  # batch_size, _height, _width, stride, stride, channels
         net = tf.reshape(net, [batch_size, _height, _width, stride * stride * channels], name='reorg')
+    return net
+
+
+def reorg_layer(net, stride=2, name='reorg'):
+    with tf.name_scope(name):
+        net = tf.extract_image_patches(net,
+                                       ksizes=[1, stride, stride, 1],
+                                       strides=[1, stride, stride, 1],
+                                       rates=[1, 1, 1, 1],
+                                       padding='VALID')
     return net
